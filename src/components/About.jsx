8aa5d5d57c1fc5b62cb760f35e1ee2906da24ad8
staystrongbg <staystrongbg@gmail.com>
2022-12-06 17:22:10 +0100

    redirect | not authorized

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,7 @@ const About = () => {
   return (
     <>
       <h1>About page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-        perspiciatis animi fugiat ullam numquam velit non? Minima soluta,
-        ratione saepe hic at iste, incidunt eos dolor odit mollitia officia
-        nesciunt.
-      </p>
+      <p>Stytch Auth System</p>
     </>
   );
 };
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Input, InputContainer } from '../App';
 import { useStytch } from '@stytch/react';
+import { useUserContext } from '../context/userContext';
 import { useNavigate } from 'react-router-dom';
-
 const ResetPassword = () => {
+  const { currentUser } = useUserContext();
   const [newPassword, setNewPassword] = useState('');
+
+  const navigate = useNavigate();
 
   const stytchClient = useStytch();
 
@@ -25,6 +28,11 @@ const ResetPassword = () => {
       .catch((err) => console.log(err));
   };
 
+  useEffect(() => {
+    if (currentUser === null) {
+      navigate('/');
+    }
+  }, [currentUser]);
   return (
     <>
       <header>
